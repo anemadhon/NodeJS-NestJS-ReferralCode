ab691d55118a09cb090cc0474bda5794c089396e
anemadhon <anemadhon@gmail.com>
2022-10-25 04:01:53 +0700

    add new config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const prismaService = app.get(PrismaService);
+
+  await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
 }
 bootstrap();
