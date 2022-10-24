010110f707cbfe0c5736d9870add2182d83c8362
anemadhon <anemadhon@gmail.com>
2022-10-24 22:39:22 +0700

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ReferralModule } from './referral/referral.module';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ReferralModule, UserModule],
+  imports: [ReferralModule, UserModule, PrismaModule],
 })
 export class AppModule {}
