840b435cf942e1b399466068f27b07b3a9c545e4
anemadhon <anemadhon@gmail.com>
2022-10-25 02:35:03 +0700

    implement interceptor for success responses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ReferralModule } from './referral/referral.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ResponseSuccessInterceptor } from './response.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [ReferralModule, UserModule, PrismaModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseSuccessInterceptor,
+    },
+  ],
 })
 export class AppModule {}
