6d89e04b92b3fc42a8b61affebe147f6aff98851
anemadhon <anemadhon@gmail.com>
2022-10-24 21:04:48 +0700

    add validator to dto

--- src/referral/dto/referral.dto.ts
+++ src/referral/dto/referral.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class ReferralDto {
+  @IsString()
   code: string;
+
+  @IsString()
   type: string;
+
+  @IsString()
   description: string;
 }
