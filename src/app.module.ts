e03b55186ee9067345e56f82692e225c8e62b2ae
anemadhon <anemadhon@gmail.com>
2022-10-25 07:14:24 +0700

    register config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,18 @@ import { ResponseSuccessInterceptor } from './response.interceptor';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { ResponseErrorExceptionFilter } from './response.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ReferralModule, UserModule, PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ReferralModule,
+    UserModule,
+    PrismaModule,
+    AuthModule,
+  ],
   providers: [
     { provide: APP_FILTER, useClass: ResponseErrorExceptionFilter },
     {
