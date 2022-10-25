2132831dbf37cda08b290dc3bcf671df1b915258
anemadhon <anemadhon@gmail.com>
2022-10-25 08:01:20 +0700

    add swagger property in dto

--- src/referral/dto/referral.dto.ts
+++ src/referral/dto/referral.dto.ts
@@ -1,12 +1,16 @@
 import { IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class ReferralDto {
+	@ApiProperty()
 	@IsString()
 	code: string
 
+	@ApiProperty()
 	@IsString()
 	type: string
 
+	@ApiProperty()
 	@IsString()
 	description: string
 }
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,12 +1,16 @@
 import { IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class UserDto {
+	@ApiProperty()
 	@IsString()
 	name: string
 
+	@ApiProperty()
 	@IsString()
 	username: string
 
+	@ApiProperty()
 	@IsString()
 	password: string
 }
